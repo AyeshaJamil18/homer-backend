451d30025a280b11cd8977d0cfee32cb03fb0b98
jonase <jonas.engeler@tum.de>
2020-07-17 13:51:44 +0200

    Implement creation of leaderboards

--- src/controllers/leaderboard.js
+++ src/controllers/leaderboard.js
@@ -9,6 +9,23 @@ const apiGetOwnData = (req, res) => {
         .then(leaderboard => res.status(200).json(leaderboard));
 };
 
+const createLeaderboardIfNotExistent = (leaderboardIdentifier, res) => {
+    leaderboardModel.exists({identifier : leaderboardIdentifier}).then(exists => {
+        if (exists) {
+            logger.info("leaderboard for " + leaderboardIdentifier + " already existed")
+            res.status(200).send();
+        } else {
+            leaderboardModel.create({
+                identifier: leaderboardIdentifier
+            }).then(() => {
+                res.status(200).send();
+            }).catch(err => {
+                logger.error(err);
+                res.status(500).send();
+            });
+        }
+    });
+}
 
 module.exports = {
     apiGetOwnData,
